fc92205173799af690876f8c313604d2845782f6
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-21 23:37:27 +0530

    Installed Typewrite effect pacakage

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -3,8 +3,14 @@
 import { useAuth } from "@clerk/nextjs"
 
 const LandingHero = () => {
+  const { isSignedIn } = useAuth();
+
   return (
-    <div>LandingHero</div>
+    <div className="text-white font-bold py-36 text-center space-y-5">
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+        <h1>The Best AI Tool for</h1>
+      </div>
+    </div>
   )
 }
 
