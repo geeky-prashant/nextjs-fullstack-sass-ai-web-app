4e75f13420691dc9b32d91e2330a41b72c1c3e0f
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-21 23:22:26 +0530

    Completed LandingNavbar component

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -1,6 +1,33 @@
+"use client"
+
+import { useAuth } from "@clerk/nextjs"
+import { Montserrat } from "next/font/google"
+import Image from "next/image"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { Button } from "./ui/button"
+
+const font = Montserrat({
+  weight: "600",
+  subsets: ["latin"]
+})
+
 const LandingNavbar = () => {
+  const { isSignedIn } = useAuth()
+
   return (
-    <div>LandingNavbar</div>
+    <nav className="p-4 bg-transparent flex items-center justify-between">
+      <Link href="/dashboard" className="flex items-center text-2xl font-bold text-white">
+        365<span className={cn("text-2xl font-semibold text-yellow-500", font.className)}>SASS</span>
+      </Link>
+
+      <div className="flex items-center gap-x-2">
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+          <Button variant="outline" className="rounded-full">Get Started</Button>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
