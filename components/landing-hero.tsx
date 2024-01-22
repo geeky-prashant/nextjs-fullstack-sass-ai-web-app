15772944e7c1a2849213ae94e930acbb5ef8f36e
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-22 23:41:00 +0530

    Installed crisp web sdk

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -9,15 +9,15 @@ const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="text-white font-bold py-24 md:py-32 text-center space-y-5">
-      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+    <div className="text-white font-bold py-24 md:py-32 text-center space-y-5 px-4">
+      <div className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <Typewriter
             options={{
               strings: [
                 "Chatbot",
-                "Image Generation",
+                "Photo Generation",
                 "Video Generation",
                 "Music Generation",
                 "Code Generation",
