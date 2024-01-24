814b37a7c35c8957d181a134a8a08eaaa645264c
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-24 21:37:31 +0530

    Designed the loader component

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image"
+
 export const Loader = () => {
   return (
-    <div>Loading...</div>
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="w-10 h-10 relative animate-spin">
+        <Image
+          alt="spinner"
+          fill
+          src="/spinner.png"
+        />
+      </div>
+      <p className="text-sm text-muted-foreground">Something is coming...</p>
+    </div>
   )
 }
