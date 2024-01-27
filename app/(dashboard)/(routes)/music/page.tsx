e3393ae1035d098e9d6d68c30737ce8d11b8b84e
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-27 23:22:22 +0530

    Completed music generation AI model

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -105,9 +105,13 @@ const MusicPage = () => {
               <Empty label="No music generated" />
             )
           }
-          <div>
-            Music will be generated here
-          </div>
+          {
+            music && (
+              <audio controls className="w-full mt-8">
+                <source src={music} />
+              </audio>
+            )
+          }
         </div>
       </div>
     </div>
