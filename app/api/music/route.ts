d9257105716410a54a33d813c7321111eab8de38
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-27 23:16:12 +0530

    Wrote the API for music generation

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -20,7 +20,16 @@ export async function POST(req: Request) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    return NextResponse.json();
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
+
+    return NextResponse.json(response);
   } catch (error) {
     console.log("[MUSIC_ERROR]", error);
     return new NextResponse("Internal error", { status: 500 });
