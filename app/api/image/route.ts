0c95e57bfb9f7c10859ba0bfa32518f9ed1b6112
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-30 23:58:48 +0530

    Wrote the API for image generation

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { messages } = body;
+    const { prompt, amount = 1, resolution = "512x512" } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -19,18 +19,27 @@ export async function POST(req: Request) {
       return new NextResponse("OPENAI API Key not configured", { status: 500 });
     }
 
-    if (!messages) {
-      return new NextResponse("Messages are required", { status: 400 });
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages,
+    if (!amount) {
+      return new NextResponse("Amount is required", { status: 400 });
+    }
+
+    if (!resolution) {
+      return new NextResponse("Resolution is required", { status: 400 });
+    }
+
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
     });
 
-    return NextResponse.json(response.choices[0].message);
+    return NextResponse.json(response.data.data);
   } catch (error) {
-    console.log("[CONVERSATION_ERROR]", error);
+    console.log("[IMAGE_ERROR]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
