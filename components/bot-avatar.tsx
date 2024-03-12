42f5dd60622540a40fb1f0533773e7d4bf47fe66
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-03-12 19:45:09 +0530

    Fixed vercel deployment issue

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -1,9 +1,9 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export const BotAvatar = () => {
   return (
-    <Avatar className="bg-yellow-100 rounded-full h-8 w-8">
+    <Avatar className="h-8 w-8 rounded-full bg-yellow-100">
       <AvatarImage className="p-1" src="/chatbot.svg" />
     </Avatar>
-  )
-}
+  );
+};
